fc8a4082a8a06bb799112568fe3382a667aa545c
yasa22 <spartyasaga22@gmail.com>
2024-02-01 18:41:40 +0100

    terminado

--- saludos.php
+++ saludos.php
@@ -1,6 +1,11 @@
 <?php
-
-for ($i = 0; $i < $_GET['num']; $i++) {
-    echo "Hola mundo<br>";
-}
+    echo "<h1>Me has pedido que te salude " . $_GET['num'] . " veces</h1>";
+    if ( $_GET['num'] == 42){
+        echo "<p>En realidad, esa es la respuesta a la pregunta final sobre la vida, el univefso y todo lo demás.</p>";
+    }
+    else {
+        for ($i = 0; $i < $_GET['num']; $i++) {
+            echo $i . " - Hola mundo<br>";
+        }
+    }
 ?>
